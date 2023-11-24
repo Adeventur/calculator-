4831d6ae6d1be0916ed8e0286696f602ec4a1b10
ADEBISI WASIU <105849469+Adeventur@users.noreply.github.com>
2023-11-24 10:25:49 +0100

    add files

---
+++ js/script.js
@@ -0,0 +1,30 @@
+// function to add
+function add(number1, number2){
+    return number1 + number2;
+}
+
+// modulous function
+function modulous(number1,number2){
+    return number1 % number2;
+}
+
+// division function
+function division(number1,number2){
+    return number1 / number2;
+}
+
+// multiplicatio function
+function multiplication(number1,number2){
+    return number1 * number2;
+}
+// subtraction function
+function subtraction(number1,number2){
+    return number1 - number2;
+}
+const number1 = parseInt(prompt("enter a number:"));
+const number2 = parseInt(prompt("enter another number:"));
+
+// alert(add(number1,number2));
+// alert(modulous(number1,number2));
+// alert(subtraction(number1,number2));
+alert(multiplication(number1,number2));
